edec7569207ef7b9e9b50f3dc30a944f26669bdd
sky <hcomeon@gmail.com>
2022-08-12 16:34:20 +0800

    页面布局

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,13 @@ import router from './router/index';
 </template>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
