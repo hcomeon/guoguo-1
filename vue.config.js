67d2a7a199e1c76fc876df82fda98d28d5c4e24d
sky <hcomeon@gmail.com>
2022-08-14 10:38:50 +0800

    svg 自带 fill 属性问题

--- vue.config.js
+++ vue.config.js
@@ -10,13 +10,20 @@ module.exports = {
       .test(/\.svg$/)
       .include.add(dir)
       .end() // 包含 icons 目录
-      .use("svg-sprite-loader")
-      .loader("svg-sprite-loader")
+      .use("svg-sprite-loader-mod")
+      .loader("svg-sprite-loader-mod")
       .options({ extract: false })
+      .end()
+      .use("svgo-loader")
+      .loader("svgo-loader")
+      .tap((options) => ({
+        ...options,
+        plugins: [{ removeAttrs: { attrs: "fill" } }],
+      }))
       .end();
     config
       .plugin("svg-sprite")
-      .use(require("svg-sprite-loader/plugin"), [{ plainSprite: true }]);
+      .use(require("svg-sprite-loader-mod/plugin"), [{ plainSprite: true }]);
     config.module.rule("svg").exclude.add(dir); // 其他 svg loader 排除 icons 目录
   },
 };
